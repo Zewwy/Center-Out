729d607e589d9ce43d14622ceb24d86e4a6a4c6d
Zewwy <zew@live.ca>
2019-02-01 01:15:16 -0600

    Updated code to support NoNewLine Cmdlet Parameter

--- Centeralize.ps1
+++ Centeralize.ps1
@@ -1,24 +1,39 @@
-#Function to Centralize Write-Host Output, Just take string variable parameter and pads it
-#Nerd Level over 9000!!! Ad-hoc Polymorphic power time!!
+#$pswheight = (get-host).UI.RawUI.MaxWindowSize.Height
 $pswwidth = (get-host).UI.RawUI.MaxWindowSize.Width
-function Centralize()
+function Centeralize()
 {
   param(
   [Parameter(Position=0,Mandatory=$true)]
   [string]$S,
   [Parameter(Position=1,Mandatory=$false,ParameterSetName="color")]
-  [string]$C
+  [string]$C,
+  [Parameter(Mandatory=$false)]
+  [switch]$NoNewLine = $false
   )
     $sLength = $S.Length
     $padamt =  "{0:N0}" -f (($pswwidth-$sLength)/2)
     $PadNum = $padamt/1 + $sLength #the divide by one is a quick dirty trick to covert string to int
     $CS = $S.PadLeft($PadNum," ").PadRight($PadNum," ") #Pad that shit
-    if ($C) #if variable for color exists run below
-    {    
-        Write-Host $CS -ForegroundColor $C #write that shit to host with color
-    }
-    else #need this to prevent output twice if color is provided
+    if (!$NoNewLine)
     {
-        $CS #write that shit without color
+        if ($C) #if variable for color exists run below
+        {    
+            Write-Host $CS -ForegroundColor $C #write that shit to host with color
+        }
+        else #need this to prevent output twice if color is provided
+        {
+            $CS #write that shit without color
+        }
+    }
+    else
+    {
+        if ($C) #if variable for color exists run below
+        {    
+            Write-Host $CS -ForegroundColor $C -NoNewLine #write that shit to host with color
+        }
+        else #need this to prevent output twice if color is provided
+        {
+            Write-Host $CS -NoNewLine #write that shit without color
+        }
     }
 }
